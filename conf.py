5631df9b1c88ea8f0ab98304385153028b1698c5
yusovadarya <yusovadarya@gmail.com>
2021-05-12 15:49:56 +0500

    0.2.7

--- conf.py
+++ conf.py
@@ -54,9 +54,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'press'
+html_theme = 'sphinx_material'
 # html_theme_path = 'sphinx_press_theme'
-html_logo = '_static/docone-logo.png'
+# html_logo = '_static/docone-logo.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
